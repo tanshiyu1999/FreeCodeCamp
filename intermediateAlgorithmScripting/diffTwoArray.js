50db2d7772813e6176590a19d4866182262a6792
Tan Shi Yu <42184874+tanshiyu1999@users.noreply.github.com>
2020-05-12 13:51:42 +0800

    diff Two Arrays

---
+++ intermediateAlgorithmScripting/diffTwoArray.js
@@ -0,0 +1,18 @@
+function diffArray(arr1, arr2) {
+  var newArr = [];
+
+  let checkArr1 = arr1.filter(i => !arr2.includes(i));
+  let checkArr2 = arr2.filter(i => !arr1.includes(i));
+  newArr = newArr.concat(checkArr1, checkArr2)
+  console.log(newArr)
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+
+/* Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+Note
+You can return the array with its elements in any order. */
