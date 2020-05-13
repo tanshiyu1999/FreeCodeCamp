1bfe57cac84c1a149312c755f27c6b9f7e3a1ce4
Tan Shi Yu <42184874+tanshiyu1999@users.noreply.github.com>
2020-05-13 17:24:11 +0800

    corrected

--- intermediateAlgorithmScripting/argumentsOptional.js
+++ intermediateAlgorithmScripting/argumentsOptional.js
@@ -1,14 +1,18 @@
 function addTogether(...args) {
-  console.log(typeof args[1])
 
-  if (args.every( n => typeof n === number)) {
+  if (!args.every( n => typeof n === 'number')) {
     console.log("undefined")
     return undefined;
   } else if (args.length == 1) {
     return function(b) {
-      args.push(b);
-      console.log(args.reduce((a,b) => a + b));
-      return args.reduce( (a,b) => a + b,0);
+      if (typeof b === 'number') {
+        args.push(b);
+        console.log(args.reduce((a,b) => a + b));
+        return args.reduce( (a,b) => a + b,0);
+      } else {
+        return undefined
+      }
+
     }
   } else if (args.length == 2) {
     console.log(args.reduce((a,b) => a + b));
@@ -17,3 +21,11 @@ function addTogether(...args) {
 }
 
 addTogether(2, "3")
+
+/* Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+Calling this returned function with a single argument will then return the sum:
+var sumTwoAnd = addTogether(2);
+sumTwoAnd(3) returns 5.
+If either argument isn't a valid number, return undefined.
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/arguments-optional*/
